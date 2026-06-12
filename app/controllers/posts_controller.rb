cf4130a8c50dab9dff94d903d8ba08838d3ab22e
Enes Bgn <enesbagin@gmail.com>
2026-06-12 11:14:31 +0300

    Add post edit form with partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update]
+
   def index
     @posts = Post.published
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -21,7 +22,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :article, :status)
