2d182984bca1afe4b473582a551443d7713dd105
Enes Bgn <enesbagin@gmail.com>
2026-06-12 11:37:36 +0300

    Add category scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_30_060930) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_12_083652) do
+  create_table "categories", force: :cascade do |t|
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "article"
     t.datetime "created_at", null: false
