a00826c1a6a975987fa45f8f0875fa251b99cb4d
Enes Bgn <enesbagin@gmail.com>
2026-06-13 19:13:54 +0300

    Add user validations and friendly URLs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :timeoutable
+
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
+  enum :gender, {
+    secret: 0,
+    male: 1,
+    female: 2
+  }
+
+  normalizes :firstname, with: -> firstname { firstname.squish.titlecase }
+  normalizes :lastname, with: -> lastname { lastname.squish.titlecase }
+
+  validates :firstname, presence: true, length: { maximum: 255 }
+  validates :lastname, presence: true, length: { maximum: 255 }
+  validates :username, presence: true, length: { maximum: 32 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :gender, inclusion: { in: genders.keys }
+
+  def fullname
+    "#{firstname} #{lastname}"
+  end
 end
