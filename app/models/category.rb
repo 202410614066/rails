21e881eaae0e883fcc4690aa82e781a5debff5cf
Enes Bgn <enesbagin@gmail.com>
2026-06-13 18:59:12 +0300

    Add validations and normalization

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :posts
+
+  normalizes :name, with: -> name { name.squish.titleize }
+
+  validates :name, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   enum :status, { draft: 0, published: 1, inactive: 2 }
 
   has_and_belongs_to_many :categories
+
+  normalizes :title, with: -> title { title.squish.titlecase }
+
+  validates :title, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :article, presence: true, length: { maximum: 65_535 }
+  validates :status, inclusion: { in: statuses.keys }
 end
