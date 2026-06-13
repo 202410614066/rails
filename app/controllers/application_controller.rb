8334a966fef932c8f670d4bd2bd4d6a2c13af5b2
Enes Bgn <enesbagin@gmail.com>
2026-06-13 19:17:17 +0300

    Customize Devise views and permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,22 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
   around_action :switch_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :email, :username, :gender])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
